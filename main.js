a14e5ced5b8bb4339556e91f8751df7b50503d7c
ksawerygroup <ksawerygroup@gmail.com>
2018-12-02 14:22:15 +0100

    add file directory

--- main.js
+++ main.js
@@ -6,21 +6,38 @@ canvas.height = window.innerHeight;
 ctx.strokeStyle = '#BADA55';
 ctx.lineJoin = 'round'; // połączenie lini
 ctx.lineCap = 'round'; // koniec lini
+ctx.lineWidth = 10;
 
 let isDrawing = false;
 let lastX = 50;
 let lastY = 50;
+let hue = 0;
 
 function draw(e) {
   if (!isDrawing) return;
+  ctx.strokeStyle = `hsl(${hue}, 100% ,50%)`
   ctx.beginPath();
   ctx.moveTo(lastX, lastY); // start rysowania
   ctx.lineTo(e.offsetX, e.offsetY)
   ctx.stroke();
   [lastX, lastY] = [e.offsetX, e.offsetY]; // skrót lastX = e.offsetX
+  hue++;
+  if (hue > 360) {
+    hue = 0;
+  };
+  ctx.lineWidth++;
+  if (ctx.lineWidth > 100) {
+    ctx.lineWidth = 10
+  }
 }
 
 canvas.addEventListener('mousemove', draw);
-canvas.addEventListener('mousedown', () => isDrawing = true);
-canvas.addEventListener('mouseup', () => isDrawing = false);
+canvas.addEventListener('mousedown', (e) => {
+  isDrawing = true;
+  [lastX, lastY] = [e.offsetX, e.offsetY];
+});
+canvas.addEventListener('mouseup', () => {
+  isDrawing = false;
+  hue = 0;
+});
 canvas.addEventListener('mouseout', () => isDrawing = false);
